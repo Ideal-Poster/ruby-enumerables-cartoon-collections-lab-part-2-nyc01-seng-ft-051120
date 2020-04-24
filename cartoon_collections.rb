a6886b3914ed8e2d82070c2131f231ea8aea45d3
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-24 23:34:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,13 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  array.map { |n| "#{n.capitalize}!" }
+  array.map { |s| "#{s.capitalize}!" }
 end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.any? {||}
+  planeteer_calls.any? {|s| s.length > 4}
 end
 
 def find_valid_calls(planeteer_calls)
